65834812511ba110eae4a4b7f592a9c5e4bbd267
zach.hall <29452017+zachhall1234@users.noreply.github.com>
2026-05-21 12:51:36 +0100

    DFI-2115 scala fmt

--- app/controllers/FileContainsVirusController.scala
+++ app/controllers/FileContainsVirusController.scala
@@ -23,7 +23,7 @@ import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.FileContainsVirusView
 
-class FileContainsVirusController @Inject()(
+class FileContainsVirusController @Inject() (
   override val messagesApi: MessagesApi,
   identify: IdentifierAction,
   val controllerComponents: MessagesControllerComponents,
