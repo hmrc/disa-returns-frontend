9c8942f109210360d9553c3c913118995a1f772e
zach.hall <29452017+zachhall1234@users.noreply.github.com>
2026-06-03 15:32:03 +0100

    DFI-2133 Create declaration view

---
+++ test/controllers/DeclarationControllerSpec.scala
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.SpecBase
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import views.html.DeclarationView
+import viewmodels.DeclarationViewModel
+
+class DeclarationControllerSpec extends SpecBase {
+
+  "DeclarationController" - {
+
+    "must return OK and the correct view when nilReturn is false" in {
+
+      val application =
+        applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+
+        val request =
+          FakeRequest(GET, routes.DeclarationController.onPageLoad(false).url)
+
+        val result =
+          route(application, request).value
+
+        val view =
+          application.injector.instanceOf[DeclarationView]
+
+        val expectedView =
+          view(DeclarationViewModel(false))(request, messages(application))
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual expectedView.toString
+      }
+    }
+
+    "must return OK and the correct view when nilReturn is true" in {
+
+      val application =
+        applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+
+        val request =
+          FakeRequest(GET, routes.DeclarationController.onPageLoad(true).url)
+
+        val result =
+          route(application, request).value
+
+        val view =
+          application.injector.instanceOf[DeclarationView]
+
+        val expectedView =
+          view(DeclarationViewModel(true))(request, messages(application))
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual expectedView.toString
+      }
+    }
+  }
+}
